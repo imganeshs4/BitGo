34b5bae4560af8b9d7ad3ca716633d3ea1194447
Ruthvik Vijayakumar <coderuth@outlook.com>
2017-04-08 16:53:04 +0530

    Added ViewPager

---
+++ app/src/main/java/ml/coderuth/bitgo/bit/BitActivity.java
@@ -0,0 +1,48 @@
+package ml.coderuth.bitgo.bit;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.nitrico.mapviewpager.MapViewPager;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import ml.coderuth.bitgo.R;
+import ml.coderuth.bitgo.Utils;
+
+
+public class BitActivity extends AppCompatActivity implements MapViewPager.Callback {
+
+    private ViewPager viewPager;
+    private MapViewPager mvp;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bit);
+
+        SupportMapFragment map = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setPageMargin(Utils.dp(this, 18));
+        Utils.setMargins(viewPager, 0, 0, 0, Utils.getNavigationBarHeight(this));
+
+        mvp = new MapViewPager.Builder(this)
+                .mapFragment(map)
+                .viewPager(viewPager)
+                .position(2)
+                .adapter(new BitAdapter(getSupportFragmentManager()))
+                .callback(this)
+                .build();
+    }
+
+    @Override
+    public void onMapViewPagerReady() {
+        mvp.getMap().setPadding(
+                0,
+                Utils.dp(this, 40),
+                Utils.getNavigationBarWidth(this),
+                viewPager.getHeight() + Utils.getNavigationBarHeight(this));
+    }
+
+}
---
+++ app/src/main/java/ml/coderuth/bitgo/bit/BitFragment.java
@@ -0,0 +1,58 @@
+package ml.coderuth.bitgo.bit;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ml.coderuth.bitgo.R;
+
+public class BitFragment extends Fragment {
+
+    private Toolbar toolbar;
+    private int index;
+
+    public BitFragment() { }
+
+    public static BitFragment newInstance(int i) {
+        BitFragment f = new BitFragment();
+        Bundle args = new Bundle();
+        args.putInt("INDEX", i);
+        f.setArguments(args);
+        return f;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_bin, container, false);
+        toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Bundle args = getArguments();
+        if (args != null) index = args.getInt("INDEX", 0);
+
+        ViewCompat.setElevation(getView(), 10f);
+        ViewCompat.setElevation(toolbar, 4f);
+
+        toolbar.setTitle(BitAdapter.PAGE_TITLES[index]);
+        toolbar.inflateMenu(R.menu.fragment);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().onBackPressed();
+            }
+        });
+    }
+
+}
